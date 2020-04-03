5605fade2dcb7ad5078a5c7d9479b4cacd6eea46
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-03 17:42:45 +0430

    defining and implementing `anagram_1()` func

--- code_snippets/01_Array_Sequences/#03.py
+++ code_snippets/01_Array_Sequences/#03.py
@@ -20,3 +20,12 @@
         up the same number of times in both strings), then they are anagrams of each other.
 
 """
+
+
+def anagram_1(s1: str, s2: str) -> bool:
+    "Implementation of first solution"
+    # Remove whitespaces and lowercase all letters
+    s1 = s1.replace(" ", "").lower()
+    s2 = s2.replace(" ", "").lower()
+
+    return sorted(s1) == sorted(s2)
