66c9300be672d7de96915f69d3ebc6bedd96c74c
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-03 17:30:11 +0430

    Adding initial docstring

---
+++ code_snippets/01_Array_Sequences/#03.py
@@ -0,0 +1,15 @@
+"""
+    Anagram Check
+
+    Problem:
+        Given two strings, check to see if they are anagrams. An anagram is when is two strings
+        can be written using the exact same letters(so you can just rearrange the letters to g-
+        et a different phrase or word).
+
+        For example:
+            "public relations" is an anagram of "crap built on lies"
+            "client eastwood" is an anagram of "old west action"
+
+    Note:
+        Ignore spaces and capitalization. So "d go" is an anagram of "God" and "dog" and "o d g"
+"""
