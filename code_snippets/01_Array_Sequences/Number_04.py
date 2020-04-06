89c7312b533251b96622bde07add37a79b011a7a
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-06 14:59:47 +0430

    Adding initial docstring

---
+++ code_snippets/01_Array_Sequences/Number_04.py
@@ -0,0 +1,17 @@
+"""
+    Array Pair Sum
+
+    Problem:
+        Given an integer array, output all the unique pairs that sum up to a specific value k.
+        So the input:
+
+            pair_sum([1, 3, 2, 2], 4)
+
+        Would return 2 pairs:
+
+            (1, 3)
+            (2, 2)
+
+    Note: For testing purposes, change your function so it outputs the number of pairs.
+
+"""
