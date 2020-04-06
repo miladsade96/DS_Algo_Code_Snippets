ee832d004aa5ad9c73ab103a06f18f0e845db593
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-06 15:28:31 +0430

    Adding solution to docstring

--- code_snippets/01_Array_Sequences/Number_04.py
+++ code_snippets/01_Array_Sequences/Number_04.py
@@ -14,4 +14,10 @@
 
     Note: For testing purposes, change your function so it outputs the number of pairs.
 
+    Solution:
+        The O(n) algorithm uses the set data structure. We perform a linear pass from the
+        beginning and for each element we check whether k-element is in the set of seen numbers.
+        if it is, then we found a pair of sum k and add it to the output. if not, this element
+        does't belong to a pair yet, and we add it to the set of seen element
+
 """
