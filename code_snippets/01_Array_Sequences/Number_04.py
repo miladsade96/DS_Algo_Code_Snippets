babf2074794985eb5e27f2e8b4bb5e413b19642f
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-06 15:49:30 +0430

    implementing second phase of func

--- code_snippets/01_Array_Sequences/Number_04.py
+++ code_snippets/01_Array_Sequences/Number_04.py
@@ -32,3 +32,11 @@ def pair_sum(arr: list, k: int) -> int:
     # Sets for tracking
     seen = set()
     output = set()
+
+    for num in arr:
+        target = k - num
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add((min(num, target), max(num, target)))
+    return len(output)
