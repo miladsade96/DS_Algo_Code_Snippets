a228fa88d9cea95df118878b4692afb871e3f09c
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-08 13:27:43 +0430

    Adding comparing for loop

--- code_snippets/01_Array_Sequences/Number_06.py
+++ code_snippets/01_Array_Sequences/Number_06.py
@@ -31,3 +31,9 @@ def large_cont_sum(arr: list) -> int:
 
     # defining max and current sum and assign first element to them
     max_sum = current_sum = arr[0]
+
+    for num in arr[1:]:
+        current_sum = max(current_sum + num, num)
+        max_sum = max(current_sum, max_sum)
+
+    return max_sum
