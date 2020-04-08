8a598d3c0cb5719320a42f313b7912bc88210d8c
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-08 09:22:50 +0430

    Adding solution section into initial docstring

--- code_snippets/01_Array_Sequences/Number_05.py
+++ code_snippets/01_Array_Sequences/Number_05.py
@@ -14,4 +14,19 @@
 
         output:
             5 is the missing number.
+
+    Solution:
+        The naive solution is go through every element in the second array and check whe-
+        -ther it appears in the first array. Note that there may be duplicate elements in
+        the arrays so we should pay special attention to it. The complexity of this appr-
+        -oach is O(N^2), since we would need two for loops.
+
+        A more efficient solution is to sort the first array, so while checking whether an
+        element in the first array appears in the second, we can do binary search. But we
+        should still be careful about duplicate elements. The complexity is O(NlogN).
+
+        If we don’t want to deal with the special case of duplicate numbers, we can sort
+        both arrays and iterate over them simultaneously. Once two iterators have different
+        values we can stop. The value of the first iterator is the missing element. This
+        solution is also O(NlogN).
 """
