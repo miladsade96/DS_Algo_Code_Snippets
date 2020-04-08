cf6277ef9a32df71ed584c56a3f48bdd5c25a369
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-08 08:26:03 +0430

    Adding initial docstring

---
+++ code_snippets/01_Array_Sequences/Number_05.py
@@ -0,0 +1,17 @@
+"""
+    Find The Missing Element
+
+    Problem:
+        Consider an array of non-negative integers. A second array is formed by shuffling
+        the elements of the first array and deleting a random element. Given these two a-
+        -rrays, find which element is missing in second array.
+
+        Here is an example of input, the first array is shuffled  and the number 5 is re-
+        moved to construct the second array.
+
+        Input:
+            finder([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6])
+
+        output:
+            5 is the missing number.
+"""
