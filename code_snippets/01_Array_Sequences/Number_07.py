28b05774a36acd6478aa695b0701205253c71ec9
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-11 12:50:00 +0430

    implementing the algorithm of rev_word_3() func

--- code_snippets/01_Array_Sequences/Number_07.py
+++ code_snippets/01_Array_Sequences/Number_07.py
@@ -48,3 +48,11 @@ def rev_word_3(s: str) -> str:
     spaces = [" "]
 
     i = 0
+    while i < length:
+        if s[i] not  in spaces:
+            word_start = i
+            while i < length and s[i] not in spaces:
+                i += 1
+            words.append(s[word_start: i])
+        i += 1
+    return " ".join(reversed(words))
