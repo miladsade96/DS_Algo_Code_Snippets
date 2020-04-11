8b4be249c669b30882fbb60e5ba398c3eb1be94d
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-11 12:03:15 +0430

    Adding solution part_1

--- code_snippets/01_Array_Sequences/Number_07.py
+++ code_snippets/01_Array_Sequences/Number_07.py
@@ -12,4 +12,8 @@
                 "   space here" and "space here     "
           both become:
                 "here space"
+
+    Solution:
+        We could take advantage of python's abilities and solve the problem with
+        the use of split() and some slicing or use of reversed.
 """
