f9b7e07b31eaf4fdd6b52b910e9572d51a5b3d38
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-11 11:58:43 +0430

    Adding initial docstring

---
+++ code_snippets/01_Array_Sequences/Number_07.py
@@ -0,0 +1,15 @@
+"""
+    Sentence Reversal
+
+    Problem:
+        Given a string of words, reverse all the words.
+        For Example:
+            Input --> "This is the best"
+            Output --> "best the is This"
+
+    Note: As part of ths exercise, you should remove all leading and trailing
+          whitespaces. So that inputs such as:
+                "   space here" and "space here     "
+          both become:
+                "here space"
+"""
