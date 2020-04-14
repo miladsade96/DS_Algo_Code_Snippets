890ff7882cb9274ca73572daec71e7d3ff20c7a6
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 09:01:52 +0430

    Adding solution section to docstring

--- code_snippets/01_Array_Sequences/Number_09.py
+++ code_snippets/01_Array_Sequences/Number_09.py
@@ -5,4 +5,8 @@
         Given a string, determine if it is comprised of all unique characters.
         For example, 'abcde' has all unique characters and should return true.
         The string 'aabcde' contains duplicate characters ad should return false.
+
+    Solution:
+        First: Using set data structure and built in function.
+        Second : Using set data structure and our own look up function.
 """
