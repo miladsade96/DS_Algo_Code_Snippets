683b52523f68bf573b3c8d2f46ac1efe4e99de99
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 08:57:54 +0430

    Adding initial docstring

---
+++ code_snippets/01_Array_Sequences/Number_09.py
@@ -0,0 +1,8 @@
+"""
+    Unique characters in string
+
+    Problem:
+        Given a string, determine if it is comprised of all unique characters.
+        For example, 'abcde' has all unique characters and should return true.
+        The string 'aabcde' contains duplicate characters ad should return false.
+"""
