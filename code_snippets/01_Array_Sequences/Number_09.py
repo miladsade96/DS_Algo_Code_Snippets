831f1ca8897de9247122bbbbdb35d1423175bf79
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 09:09:47 +0430

    defining and implementing second solution

--- code_snippets/01_Array_Sequences/Number_09.py
+++ code_snippets/01_Array_Sequences/Number_09.py
@@ -19,3 +19,18 @@ def uni_char_1(s: str) -> bool:
     :return: Either True or False
     """
     return len(set(s)) == len(s)
+
+
+def uni_char_2(s: str) -> bool:
+    """
+    Implementation of second solution
+    :param s: Given string
+    :return: Either True or False
+    """
+    chars = set()
+    for let in s:
+        if let in chars:
+            return False
+        else:
+            chars.add(let)
+    return True
