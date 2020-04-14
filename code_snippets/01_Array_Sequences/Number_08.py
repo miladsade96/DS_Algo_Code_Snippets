08d96c2fa94b3451a6542bb57a5557598df25cef
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 08:13:31 +0430

    implementing last part

--- code_snippets/01_Array_Sequences/Number_08.py
+++ code_snippets/01_Array_Sequences/Number_08.py
@@ -34,3 +34,13 @@ def compress(s: str) -> str:
     last = s[0]
     count = 1
     i = 1
+
+    while i < l:
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            r = r + s[i - 1] + str(count)
+            count = 1
+        i += 1
+    r = r + s[i - 1] + str(count)
+    return r
