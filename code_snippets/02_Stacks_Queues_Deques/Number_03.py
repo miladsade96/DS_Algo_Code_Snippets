854646780adf9c6387c00900c9aa73a9028524d6
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-17 13:39:35 +0430

    Adding initial docstring

---
+++ code_snippets/02_Stacks_Queues_Deques/Number_03.py
@@ -0,0 +1,19 @@
+"""
+    Implementation of Deque
+
+    Deque methods and attributes:
+        Deque() --> Creates a new deque that is empty. It needs no parameter
+                    and returns an empty deque.
+        addFront(item) --> Adds a new item to the front of the deque. It needs
+                           the item and returns nothing.
+        addRear(item) --> Adds a new item to the rear of the deque. It needs
+                          the item and returns nothing.
+        removeFront() --> Removes the front item from the deque. It needs no
+                          parameters and returns the item. The deque is modified.
+        removeRear() --> Removes the rear item from the deque. It needs no
+                         parameters and return the item. The deque is modified.
+        isEmpty() --> Tests to see whether the deque is empty or not. It needs
+                      no parameters and returns a boolean value.
+        size() --> Returns the number of items in the deque. It needs no para-
+                   -meter and returns an integer
+"""
