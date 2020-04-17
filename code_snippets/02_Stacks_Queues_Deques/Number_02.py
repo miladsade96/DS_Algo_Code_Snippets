463b006eb7c75546984bba2dc8384276143f30e3
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-17 12:15:55 +0430

    Adding initial docstring

---
+++ code_snippets/02_Stacks_Queues_Deques/Number_02.py
@@ -0,0 +1,15 @@
+"""
+    Implementation of Queue
+
+    Queue methods and attributes:
+        Queue() --> Creates a new queue that is empty. It needs no parameters
+                    and returns an empty queue.
+        enqueue(item) --> Adds a new item to the rear of the queue. It needs
+                          the item and returns nothing.
+        dequeue() --> Removes the front item from the queue. It needs no para-
+                      -meter and returns the item. The queue is modified.
+        isEmpty() --> Tests to see whether the queue is empty or not. It needs
+                      no parameter and returns a boolean value.
+        size() --> Returns the number of items in queue. It needs no parameter
+                   and returns an integer.
+"""
