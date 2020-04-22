d0cfafacc1a9e9b9c7d3efc6d70b5bae936cce90
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-22 21:49:42 +0430

    defining dequeue method

--- code_snippets/02_Stacks_Queues_Deques/Number_05.py
+++ code_snippets/02_Stacks_Queues_Deques/Number_05.py
@@ -20,3 +20,9 @@ class TwoStackQueue(object):
 
     def enqueue(self, element):
         self.in_stack.append(element)
+
+    def dequeue(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack.pop()
