623abcea47281df5b5339e8db7e6771691a3ba1a
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-22 21:43:48 +0430

    Adding initial docstring

---
+++ code_snippets/02_Stacks_Queues_Deques/Number_05.py
@@ -0,0 +1,13 @@
+"""
+    Problem:
+        Implement a queue data structure using two stacks.
+        Given a stack class below, implement a queue class using two stacks.
+        Use a python list data structure as your stack.
+
+    solution:
+        The key insight is that a stack reverses order. A sequence of elements pushed
+        on a stack comes back in the reversed order when popped. Consequently, two
+        stacks chained together will return elements in the same order, since reversed
+        order reversed again is original order.
+
+"""
