e095df81c32bbd98dc7254f19c9334de7ba2b8a7
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-22 08:07:19 +0430

    Adding initial Docstring

---
+++ code_snippets/02_Stacks_Queues_Deques/Number_04.py
@@ -0,0 +1,15 @@
+"""
+    Balanced Parenthesis Check
+
+    Problem:
+        Given an string of opening and closing parenthesis, check whether it's
+        balanced. We have 3 types of parenthesis: round brackets -> (), square
+        brackets -> [] and curly bracket -> {}. Assume that the string doesn't
+        contain any other character than these, no spaces, words or numbers. As
+        a reminder, balanced parenthesis require every opening parenthesis to
+        be closed in the reversed order opened.
+
+        For Example:
+            input --> ({}) is balanced,     output --> True
+            input --> ({]) is'nt balanced,  output --> False
+"""
