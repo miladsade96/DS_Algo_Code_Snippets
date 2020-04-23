f07040fbe396a154df42c572256c38f922c48437
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-23 11:07:47 +0430

    Adding Pros into docstring

--- code_snippets/03_Linked_Lists/Number_01.py
+++ code_snippets/03_Linked_Lists/Number_01.py
@@ -1,5 +1,12 @@
 """
     Implementation of Singly Linked List
+
+    Pros:
+        (1) - Linked lists have constant time insertions and deletions
+              in any position, in comparison, arrays require O(n) time
+              to do the same thing.
+        (2) - Linked lists can continue to expand without having to
+              specify their size ahead of time.
 """
 
 
