7bae1b686fddf17cfde5a215a9c7f3e8f87373b8
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-25 19:45:06 +0430

    Adding solution section into docstring

--- code_snippets/03_Linked_Lists/Number_03.py
+++ code_snippets/03_Linked_Lists/Number_03.py
@@ -7,4 +7,10 @@
         if the linked list contains a cycle.
         A cycle is when a node's next point actually points back to a previous
         node in the list.
+
+    Solution:
+        To solve this problem we will have two markers traversing through the list.
+        marker_1 and marker_2. We will have both markers begin at the first node of
+        the list and traverse through the linked list. However the second marker,
+        marker_2, will move two nodes ahead from every one node that marker_1 moves.
 """
