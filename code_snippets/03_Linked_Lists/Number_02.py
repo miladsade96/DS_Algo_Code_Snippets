98868a3b7086769b4725c6c6b64d191e0c5a74f5
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-25 12:23:07 +0430

    defining doubly linked list node class and its init method

--- code_snippets/03_Linked_Lists/Number_02.py
+++ code_snippets/03_Linked_Lists/Number_02.py
@@ -1,3 +1,10 @@
 """
     Implementation of doubly linked list
 """
+
+
+class DoublyLinkedListNode(object):
+    def __init__(self, value):
+        self.value = value
+        self.next_node = None
+        self.prev_node = None
