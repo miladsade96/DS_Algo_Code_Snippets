31e7ef2bc832926652ef45e9d7089f76352b5675
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-25 13:55:17 +0430

    Adding initial docstring

---
+++ code_snippets/03_Linked_Lists/Number_03.py
@@ -0,0 +1,10 @@
+"""
+    Singly linked list cycle check
+
+    Problem:
+        Given a singly linked list, write a function which takes in the
+        first node in a singly linked list and returns a boolean indicating
+        if the linked list contains a cycle.
+        A cycle is when a node's next point actually points back to a previous
+        node in the list.
+"""
