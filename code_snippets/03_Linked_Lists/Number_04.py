ede4b0bbb1638cf8ac2d5fb26902b383174dfec5
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-05-07 08:10:00 +0430

    Adding solution section into docstring

--- code_snippets/03_Linked_Lists/Number_04.py
+++ code_snippets/03_Linked_Lists/Number_04.py
@@ -5,4 +5,15 @@
         write a function reverse a linked list in place. The function
         will take in a head of the list as input and return the new
         head of the list.
+
+    Solution:
+        Since we want to do this in place,  we want to make the function
+        operate in O(1) space, meaning we don't' want to create a new List
+        , so we will simply use the current nodes. Time wise, we can perform
+        the reversal in O(n) time.
+
+        We can reverse the list by changing the next pointer of each node.
+        Each node's next pointer should point to the previous node. In one
+        pass head to tail of our input list, we will point each node's next
+        pointer to the previous element.
 """
