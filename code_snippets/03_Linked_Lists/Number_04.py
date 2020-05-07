f0ed8c3266f65422ace8f0ae6f9f26d6947422f5
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-05-07 07:27:33 +0430

    Adding initial docstring

---
+++ code_snippets/03_Linked_Lists/Number_04.py
@@ -0,0 +1,8 @@
+"""
+    Linked List Reversal
+
+    Problem:
+        write a function reverse a linked list in place. The function
+        will take in a head of the list as input and return the new
+        head of the list.
+"""
