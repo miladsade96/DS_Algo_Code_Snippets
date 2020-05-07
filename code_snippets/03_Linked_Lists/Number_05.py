6d7da2315a491c91875dd0e7806e17af0f82c266
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-05-07 11:17:08 +0430

    Adding initial docstring

---
+++ code_snippets/03_Linked_Lists/Number_05.py
@@ -0,0 +1,12 @@
+"""
+    Linked List Nth to Last Node
+
+    Problem:
+        Write a function that takes a head node an integer value
+        n and then returns the nth to the last node in the linked
+        list.
+        For example:
+            >> target_node = nth_to_last_node(2, head)
+            >> target_node.value
+            4
+"""
