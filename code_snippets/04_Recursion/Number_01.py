a0d4f93ce761a9543dfeece5f2b35271112bdd06
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-14 11:29:42 +0430

    defining function and its docstring

--- code_snippets/04_Recursion/Number_01.py
+++ code_snippets/04_Recursion/Number_01.py
@@ -5,3 +5,11 @@
                 n! = n * (n - 1)!
                 if n = 0 --> n! = 1
 """
+
+
+def factorial(n: int) -> int:
+    """
+    Factorial function with recursion
+    :param n: given number that we gonna find its factorial
+    :return: factorial value
+    """
