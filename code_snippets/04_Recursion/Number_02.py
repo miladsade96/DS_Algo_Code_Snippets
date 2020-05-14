c004d4e0b25877fc805f0500f0081d7db4d526fc
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-14 12:27:17 +0430

    Adding initial docstring

---
+++ code_snippets/04_Recursion/Number_02.py
@@ -0,0 +1,11 @@
+"""
+    Problem:
+        Write a recursive function which takes an integer and
+        computes the cumulative sum of 0 to that integer.
+
+        For example:
+            if n = 4 --> return 4 + 3 + 2 + 1, which is 10.
+
+    Hint:
+        In this case, we have: n + (n - 1) + (n - 2) + ... + 0
+"""
