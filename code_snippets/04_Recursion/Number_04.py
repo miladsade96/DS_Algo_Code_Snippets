6428e7990d1431ec9f9827b9d9f58f60e5c74951
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-14 22:09:56 +0430

    Adding initial docstring

---
+++ code_snippets/04_Recursion/Number_04.py
@@ -0,0 +1,8 @@
+"""
+    Problem:
+        Create a function called word_split which takes in a string phrase
+        and a set list_of_words. The function then will determine if it is
+        possible to split the string in a way in which words can be made from
+        the list of words. You can assume the phrase will only contain words
+        found in a dictionary if it is completely splittable.
+"""
