ec2531c7c487e17cc7953475c6b8b2d945ccc771
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-14 13:06:03 +0430

    Adding initial docstring

---
+++ code_snippets/04_Recursion/Number_03.py
@@ -0,0 +1,12 @@
+"""
+    Problem:
+        Given an integer, create a function which returns the sum of all
+        the individual digits in that integer.
+
+        For example:
+            if n = 4321, return 4 + 3 + 2 + 1 which is 10
+
+    Hints:
+        You will need to use modulo --> 4321 % 10 = 1
+        and floor division operator --> 4321 // 10 = 432
+"""
