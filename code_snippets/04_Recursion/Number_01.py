925025104e8a550ee77ea75ca49b99dfa18363cd
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-14 11:31:22 +0430

    implementing the function

--- code_snippets/04_Recursion/Number_01.py
+++ code_snippets/04_Recursion/Number_01.py
@@ -13,3 +13,8 @@ def factorial(n: int) -> int:
     :param n: given number that we are gonna find its factorial
     :return: factorial value
     """
+    # Base case
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
