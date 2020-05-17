7adffd59ff142cfea791f0aeea0e25d492d01194
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-17 10:45:05 +0430

    Adding initial docstring

---
+++ code_snippets/04_Recursion/Number_05.py
@@ -0,0 +1,10 @@
+"""
+    Reverse A String
+
+    Problem:
+        This interview question requires you to reverse the string
+        using recursion. Make sure to think of the base case here.
+
+        Note: Do not slice(string[::-1]) or use iteration, there
+              must be a recursive call for the function.
+"""
