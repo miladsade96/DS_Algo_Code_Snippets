8a220633f7c3270f68f04d4eb2ddc81b967657a5
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-18 08:16:59 +0430

    Adding initial docstring

---
+++ code_snippets/04_Recursion/Number_06.py
@@ -0,0 +1,13 @@
+"""
+    String Permutation
+
+    Problem:
+        Given a string, write a function that uses recursion to output
+        list of all the possible permutations of that string.
+
+        For example: given s = 'abc'
+            The function should return ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+    Note: If a character is repeated, treat each occurrence as distinct. For
+          example an input of 'XXX' would return a list with 6 versions of 'XXX'.
+"""
