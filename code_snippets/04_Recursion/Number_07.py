ecf449d703357b81080a898cb122c6f4445b9378
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-22 11:32:42 +0430

    creating initial docstring

---
+++ code_snippets/04_Recursion/Number_07.py
@@ -0,0 +1,10 @@
+"""
+    Problem:
+        Implementation of fibonacci sequence in three different ways:
+            * Recursively
+            * Iteratively
+            * Dynamically (Using memoization to store results)
+
+    Note: Your function should accept a number n and return the nth
+          number of the fibonacci sequence.
+"""
