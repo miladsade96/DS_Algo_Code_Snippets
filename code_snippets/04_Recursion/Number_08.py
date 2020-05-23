677ebc21f98547a0a99b277d567fe0c6cd1e62bc
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-23 12:30:18 +0430

    Adding initial docstring

---
+++ code_snippets/04_Recursion/Number_08.py
@@ -0,0 +1,18 @@
+"""
+    Coin change problem
+
+    Problem:
+        This problem is common enough that is actually has its own wikipedia page!
+        Let's check it out: https://en.wikipedia.org/wiki/Change-making_problem)
+
+        This is a classic recursion problem: Given a target amount n and a list (array)
+        of distinct coin values, what's the fewest coins needed to make the change amount.
+
+        For Example:
+            If n = 10 and coins = 1,5,10. Then there are 4 possible ways to make change:
+                1. 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+                2. 5 + 1 + 1 + 1 + 1 + 1
+                3. 5 + 5
+                4. 10
+            With 1 coin being the minimum amount.
+"""
