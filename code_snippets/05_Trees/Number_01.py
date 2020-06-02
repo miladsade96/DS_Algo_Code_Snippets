3a629963bcef20d1490b445b60695011edffe48b
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-02 19:54:17 +0430

    defining insert_left method

--- code_snippets/05_Trees/Number_01.py
+++ code_snippets/05_Trees/Number_01.py
@@ -10,3 +10,11 @@ class BinaryTree(object):
         self.key = rootObject
         self.leftChild = None
         self.rightChild = None
+
+    def insert_left(self, newNode):
+        if self.leftChild is None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
