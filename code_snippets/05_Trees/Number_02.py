b285165774e83cdbb1fe11798706d5abde5a9eb6
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-04 08:57:55 +0430

    Adding initial docstring

---
+++ code_snippets/05_Trees/Number_02.py
@@ -0,0 +1,12 @@
+"""
+    Tree Traversals:
+        1. Preorder: We visit the first node first, thn recursively do
+            a preorder traversal of the left subtree, followed by a
+            recursive preorder traversal of the right subtree.
+        2. Inorder: We recursively do an inorder traversal on the left
+            subtree, visit the root node and finally do a recursive
+            inorder traversal of the right subtree.
+        3. Postorder: We recursively do a postorder traversal of the left
+            subtree and the right subtree followed by a visit to the root
+            node.
+"""
