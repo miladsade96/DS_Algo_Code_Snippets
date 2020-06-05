4659614b777d0d5f6426f794aae97feac1cd4ac5
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-05 21:17:37 +0430

    defining and implementing perc_up method

--- code_snippets/05_Trees/Number_03.py
+++ code_snippets/05_Trees/Number_03.py
@@ -19,3 +19,11 @@ class BinaryHeap(object):
     def __init__(self):
         self.heap_list = [0]
         self.current_size = 0
+
+    def perc_up(self, i):
+        while i // 2 > 0:
+            if self.heap_list[i] < self.heap_list[i // 2]:
+                tmp = self.heap_list[i // 2]
+                self.heap_list[i // 2] = self.heap_list[i]
+                self.heap_list[i] = tmp
+            i = i // 2
