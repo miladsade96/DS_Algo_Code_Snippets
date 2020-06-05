ef4216c9385131110870cece9d83f91c46c8ce5a
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-05 21:20:17 +0430

    defining and implementing insert method

--- code_snippets/05_Trees/Number_03.py
+++ code_snippets/05_Trees/Number_03.py
@@ -27,3 +27,8 @@ class BinaryHeap(object):
                 self.heap_list[i // 2] = self.heap_list[i]
                 self.heap_list[i] = tmp
             i = i // 2
+
+    def insert(self, k):
+        self.heap_list.append(k)
+        self.current_size = self.current_size + 1
+        self.perc_up(self.current_size)
