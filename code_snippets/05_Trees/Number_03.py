f6bc70c5cc194ede0f359d478e055d81232bd746
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-17 08:57:05 +0430

    defining and implementing build_heap method

--- code_snippets/05_Trees/Number_03.py
+++ code_snippets/05_Trees/Number_03.py
@@ -59,3 +59,11 @@ class BinaryHeap(object):
         self.heapList.pop()
         self.percDown(1)
         return retval
+
+    def build_heap(self, alist):
+        i = len(alist) // 2
+        self.current_size = len(alist)
+        self.heap_list = [0] + alist[:]
+        while i > 0:
+            self.perc_down(i)
+            i = i - 1
